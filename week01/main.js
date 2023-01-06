7781c76d1a62ac2ffa9f9e330b63fd967b008a61
spencer-rafada <spencer.byui@gmail.com>
2023-01-06 11:39:59 -0700

    init commit

---
+++ week01/main.js
@@ -0,0 +1,35 @@
+/*
+When the page loads
+    Get list of pokemon types.
+    render type list out (need type name and url to get all pokemon of that type)
+    attach a listener to respond to a click on the type.
+        when clicked it should pull the type url and retrieve the data
+        render the list of pokemon
+        style the clicked type as active
+        filter out types with no pokemon (shadow and unknown)
+
+
+*/
+
+// we have to convert the response fetch sends back into the appropriate type. In this case the API sends back json...so we process it as json and send it back
+
+
+// once we have the data, we need to do something with it. This won't always be the same thing though. With a callback (function passed into a function) we can change the behavior as needed.
+
+
+
+// keep things from happening until the DOM is ready
+// another alternative would be to add 'defer' to our script element
+
+
+// create simple html markup to display a list
+
+// create click handler
+
+// render the list
+
+// set active item
+
+// there are no pokemon in unknown and shadow. Let's remove them from the list.
+
+
