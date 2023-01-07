9d83a5fc73544e480931a14cce52e434af277ff5
spencer-rafada <spencer.byui@gmail.com>
2023-01-06 19:55:36 -0700

    added refresher class notes

--- week01/main.js
+++ week01/main.js
@@ -7,29 +7,71 @@ When the page loads
         render the list of pokemon
         style the clicked type as active
         filter out types with no pokemon (shadow and unknown)
-
-
 */
 
 // we have to convert the response fetch sends back into the appropriate type. In this case the API sends back json...so we process it as json and send it back
-
+function convertToJson(response) {
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error("bad response");
+  }
+}
 
 // once we have the data, we need to do something with it. This won't always be the same thing though. With a callback (function passed into a function) we can change the behavior as needed.
-
-
+async function getData(url, callback) {
+  const data = await fetch(url);
+  const json = await convertToJson(data);
+  console.log(json);
+  if (callback) {
+    callback(json);
+  }
+}
 
 // keep things from happening until the DOM is ready
 // another alternative would be to add 'defer' to our script element
+window.addEventListener("load", () => {
+  getData("https://pokeapi.co/api/v2/type", renderTypeList);
 
+  document
+    .getElementById("typeList")
+    .addEventListener("click", typeClickedHandler);
+});
 
 // create simple html markup to display a list
+// function renderTypeList()
+const renderTypeList = (list) => {
+  const element = document.getElementById("typeList");
+
+  const cleanList = list.results;
+
+  cleanList.forEach((item) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${item.name}`;
+    li.setAttribute("data-url", item.url);
+    element.appendChild(li);
+  });
+};
 
 // create click handler
+function typeClickedHandler(event) {
+  //   console.log(event.target);
+  //   console.log(event.currentTarget);
+  const selectType = event.target;
+  // anything data-x attribute = dataset.x
+  const url = selectType.dataset.url;
+  getData(url, renderPokeList);
+}
 
 // render the list
+const renderPokeList = (list) => {
+  const element = document.getElementById("pokeList");
+  element.innerHTML = "";
 
+  const header = document.createElement("h2");
+  header.innerHTML = `${list.name} (${list.pokemon.length})`;
+  element.appendChild(header);
+};
 // set active item
 
 // there are no pokemon in unknown and shadow. Let's remove them from the list.
-
-
